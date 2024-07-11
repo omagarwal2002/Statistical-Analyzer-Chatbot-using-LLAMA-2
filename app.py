9871472b795df2fc95e819804447730a839027f1
Om Agarwal <omagarwal241102@gmail.com>
2024-07-11 14:52:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from langchain_community.document_loaders import CSVLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
-from langchain_community.llms import CTransformers
+from langchain.llms import CTransformers
 from langchain.chains import ConversationalRetrievalChain
 import os
 import requests
