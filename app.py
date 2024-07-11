1dd7c06733517bf107c7553002065fa87e20a623
Om Agarwal <omagarwal241102@gmail.com>
2024-07-11 17:13:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -34,8 +34,7 @@ if uploaded_file :
                 'delimiter': ','})
     data = loader.load()
     #st.json(data)
-    embeddings = HuggingFaceEmbeddings(model_name='sentence-transformers/all-MiniLM-L6-v2',
-                                       model_kwargs={'device': 'cpu'})
+    embeddings = HuggingFaceEmbeddings(model_name = 'sentence-transformers/all-MiniLM-L6-v2')
 
     db = FAISS.from_documents(data, embeddings)
     db.save_local(DB_FAISS_PATH)
